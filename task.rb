8c28549fc041cd53e8e562c20772d29c45beb332
satokoyo <thebeatles.0605@gmail.com>
2020-09-13 16:20:25 +0900

    質問8の回答修正

--- task.rb
+++ task.rb
@@ -64,7 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map!(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages =  programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -84,7 +84,7 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  return "好物です" 
 end
 
 def q11
